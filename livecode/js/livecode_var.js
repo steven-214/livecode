2cabbfab1ef2f7414ca18a984af970a8a8ea747e
Moioio <tianbocvfe@gmail.com>
2021-01-15 13:45:28 +0800

    ok

--- livecode/js/livecode_var.js
+++ livecode/js/livecode_var.js
@@ -1,5 +1,8 @@
 // var getLiveCodeUrl = "http://127.0.0.1:8093/api/livecode/get";
-var getLiveCodeUrl = "http://47.104.98.26:8093/api/livecode/get";
+// var getLiveCodeUrl = "http://47.104.98.26:8093/api/livecode/get";
+var getLiveCodeUrl = "http://mat-api.aaios.com/api/livecode/get";
+
+
 //485affe92b3637584119c7d889fa124628f64040
 function loadLiveCode(tag,feedback)
 {
